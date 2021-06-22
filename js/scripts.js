4ed3817c89bb69ac3c8935f33b97a973cc41ba56
Alextw69 <lib151886@gmail.com>
2021-06-22 09:56:58 +0800

    add

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+$('body').css('background-color','green');
+
+TweenMax.to('.box',1,{
+  x : 400,
+  roration : 100
+})
+
+let age = 26;
+
+console.log(age);
--- scripts.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-$('body').css('background-color','red');
-
-let age = 26;
-
-console.log(age);
